6cd51684990d1a7b808c732ef64c92250391641b
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-02-07 13:11:43 +0100

    fix: Way to include search bar into DOM

--- src/js/components/searchBar.js
+++ src/js/components/searchBar.js
@@ -1,20 +1,28 @@
 const SearchBar = () => {
-  const searchBar = document.querySelector("#search-bar");
-  searchBar.innerHTML = `
-    <div class="flex absolute w-screen justify-center bottom-5 h-20">
-        <div class="relative p-3 h-20 backdrop-blur-sm w-[90%] max-w-md bg-white/50 z-40 rounded-3xl">
-        </div>
-    </div>
+  const app = document.getElementById("app");
+  const searchBar = document.createElement("div");
+  searchBar.classList.add(
+    "flex",
+    "absolute",
+    "bottom-5",
+    "w-[90%]",
+    "max-w-md",
+    "h-24"
+  );
 
-    <div class="flex absolute w-screen justify-center bottom-5 h-20 z-50">
-        <div class="flex justify-center relative p-5 rounded-x z-10">
-            <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-[10%]">
-                <span class="text-zinc-700 text-2xl"><i class="fa-solid fa-magnifying-glass"></i></span>
-            </div>
-            <input type="text" class="block w-[90%] text-center text-4xl bg-transparent border-b-2 border-zinc-700 placeholder-zinc-700 focus:outline-none" placeholder="Search">
+  searchBar.innerHTML = `
+    <div class="relative w-[100%] h-[100%] bg-white/40 backdrop-blur-sm rounded-3xl border"></div>
+    <div class="absolute w-[100%] h-[100%] flex justify-center items-center
+            text-xl font-semibold text-zinc-800">
+        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-[10%]">
+        <span class="text-zinc-800 text-xl"><i class="fa-solid fa-magnifying-glass"></i></span>
         </div>
+        <input type="text"
+            class="block w-[90%] text-center text-2xl bg-transparent border-b-2 border-zinc-800 placeholder-zinc-700 focus:outline-none" placeholder="Search">
     </div>
-    `;
+  `;
+
+  app.appendChild(searchBar);
 };
 
 export default SearchBar;
