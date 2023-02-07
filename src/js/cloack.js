758d0ef83a5224046770983da8c6c27bacdb2fd0
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-02-07 22:36:08 +0100

    feat: Create simple calendar and cloack

---
+++ src/js/cloack.js
@@ -0,0 +1,16 @@
+const Cloack = () => {
+  const time = document.getElementById("time");
+  function cloack() {
+    const date = new Date();
+    time.innerHTML = date.toLocaleString([], {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  }
+  setInterval(cloack, 1000);
+};
+
+export default Cloack;
