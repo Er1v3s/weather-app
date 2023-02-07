e2b915719d8e78647131b9337ac6967d8bf3151b
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-02-07 22:34:53 +0100

    fix: RWD for large devices

--- src/js/components/searchBar.js
+++ src/js/components/searchBar.js
@@ -6,19 +6,20 @@ const SearchBar = () => {
     "absolute",
     "bottom-5",
     "w-[90%]",
-    "max-w-md",
-    "h-24"
+    "max-w-lg",
+    "h-24",
+    "lg:h-28"
   );
 
   searchBar.innerHTML = `
-    <div class="relative w-[100%] h-[100%] bg-white/40 backdrop-blur-sm rounded-3xl border"></div>
-    <div class="absolute w-[100%] h-[100%] flex justify-center items-center
+    <div class="searchBar relative w-[100%] h-[100%] bg-white/40 backdrop-blur-sm rounded-3xl border"></div>
+    <div class="searchBar absolute w-[100%] h-[100%] flex justify-center items-center
             text-xl font-semibold text-zinc-800">
-        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-[10%]">
-        <span class="text-zinc-800 text-xl"><i class="fa-solid fa-magnifying-glass"></i></span>
+        <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-[5%]">
+        <span class="text-zinc-800 text-xl lg:text-2xl"><i class="fa-solid fa-magnifying-glass"></i></span>
         </div>
         <input type="text"
-            class="block w-[90%] text-center text-2xl bg-transparent border-b-2 border-zinc-800 placeholder-zinc-700 focus:outline-none" placeholder="Search">
+            class="block w-[90%] text-center text-2xl bg-transparent border-b-2 border-zinc-800 placeholder-zinc-700 focus:outline-none lg:text-4xl" placeholder="Search">
     </div>
   `;
 
