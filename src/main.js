adf375b979865656bc29b66fe6a5aa21fabdf8d7
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-02-08 18:28:19 +0100

    feat: Main file to display app for the first time

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
-const app = document.getElementById("app");
+// components
+import { SearchBar } from "./js/components/SearchBar.js";
+import { TimeDate } from "./js/components/TimeDate.js";
+import { Widgets } from "./js/components/Widgets.js";
+import { VidmoWidget } from "./js/components/VidmoWidget.js";
+import { Video } from "./js/components/Video.js";
+import { WidgetContainer } from "./js/components/WidgetContainer.js";
 
-app.innerHTML = "Hello World";
+// scripts
+import { Cloack } from "./js/cloack.js";
+import { attachData } from "./js/attachData.js";
+import { animate } from "./js/animation.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  Video();
+  WidgetContainer();
+  TimeDate();
+  Cloack();
+  SearchBar();
+  Widgets();
+  attachData();
+  VidmoWidget();
+  setTimeout(animate, 2000);
+});
