f9ec65b21845388a6425eb0920a1824b0c63e388
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-02-09 11:15:23 +0100

    feat: Error handling in video component

--- src/js/components/Video.js
+++ src/js/components/Video.js
@@ -1,6 +1,6 @@
 import { fetchingData } from "../promise.js";
 
-const Video = () => {
+const Video = async () => {
   const app = document.getElementById("app");
 
   const windowX = window.innerWidth;
@@ -14,31 +14,42 @@ const Video = () => {
 
   const video = document.createElement("video");
   video.setAttribute("autoplay", "");
-
   video.setAttribute("loop", "loop");
   video.setAttribute("muted", "muted");
   video.setAttribute("playsinline", "playinline");
   video.setAttribute("preload", "preload");
   video.setAttribute("class", "absolute w-screen h-screen -z-50 object-cover");
 
-  fetchingData.then((data) => {
-    let weather = "";
-    if (data.weather >= 200 && data.weather < 300) {
-      weather = "thunder";
-    } else if (data.weather >= 300 && data.weather < 600) {
-      weather = "rain";
-    } else if (data.weather >= 600 && data.weather < 700) {
-      weather = "snowy";
-    } else if (
-      (data.weather >= 700 && data.weather < 800) ||
-      (data.weather > 800 && data.weather < 900)
-    ) {
-      weather = "cloudy";
-    } else if (data.weather === 800) {
-      weather = "sunny";
-    }
-    video.setAttribute("src", `${weather}-${size}.mp4`);
-  });
+  await fetchingData
+    .then((data) => {
+      let weather = "";
+      if (data.err === false) {
+        if (data.weather >= 200 && data.weather < 300) {
+          weather = "thunder";
+        } else if (data.weather >= 300 && data.weather < 600) {
+          weather = "rain";
+        } else if (data.weather >= 600 && data.weather < 700) {
+          weather = "snowy";
+        } else if (
+          (data.weather >= 700 && data.weather < 800) ||
+          (data.weather > 800 && data.weather < 900)
+        ) {
+          weather = "cloudy";
+        } else if (data.weather === 800) {
+          weather = "sunny";
+        }
+      } else {
+        weather = "sunny";
+        throw new Error(data.message);
+      }
+      video.setAttribute("src", `${weather}-${size}.mp4`);
+    })
+    .catch((err) => {
+      console.error(
+        err.message +
+          " | Something went wrong trying displaying background video, check your entered data and try one more time"
+      );
+    });
 
   app.appendChild(video);
 };
