c856669ff838e599ce8c917c62a4643a9ba80884
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-02-09 11:13:55 +0100

    fix: Autocomplete using pointless hints

--- src/js/components/searchBar.js
+++ src/js/components/searchBar.js
@@ -18,7 +18,7 @@ const SearchBar = () => {
         <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-[5%]">
         <span class="text-zinc-800 text-xl lg:text-2xl"><i class="fa-solid fa-magnifying-glass"></i></span>
         </div>
-          <input id="search" type="text"
+          <input id="search" type="text" autocomplete="off"
           class="block w-[90%] text-center text-2xl bg-transparent border-b-2 border-zinc-800 placeholder-zinc-700 focus:outline-none lg:text-4xl" placeholder="Search">
     </div>
   `;
